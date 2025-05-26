34248e99325c06dac2ab1849474e3722d838a540
Kalum Gunawardhana <kguna415@gmail.com>
2025-05-26 19:07:42 +0530

    first commit

---
+++ src/main/java/edu/TodoList/TodoListApplication.java
@@ -0,0 +1,11 @@
+package edu.TodoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListApplication.class, args);
+	}
+}
